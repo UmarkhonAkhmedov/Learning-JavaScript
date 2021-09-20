855613174e8e325fdf3062212c7075e2254ad33e
UmarkhonAkhmedov <umarkhonakhmedov2000@gmail.com>
2021-09-20 08:23:02 +0500

    Learning function, loops, string methods

---
+++ JavaScript Tutorial/js/1Script.js
@@ -0,0 +1,44 @@
+// Learning Function and Loops
+// Function
+
+function fun() {
+  alert('this is a function');
+}
+ fun();
+
+function greeting(){
+  var name = prompt('What is your name?');
+  var result = 'Hello ' + name;
+  console.log(result);
+}
+greeting();
+
+function sumNumbers(num1, num2){
+  var result = num1 + num2;
+  console.log(result);
+}
+sumNumbers(10, 10);
+
+// Loop
+// While loops
+var num = 0;
+while(num < 100){
+  num += 1;
+  console.log(num);
+}
+
+// For loops
+for(let num = 0; num < 100; num++){
+  console.log(num);
+}
+
+// String methods
+// \n - new line
+let fruit = 'banana';
+console.log(fruit.length);
+console.log(fruit.indexOf('an')); // it gives starting of index
+console.log(fruit.slice(2, 6)); // slicing from 2 to 6
+console.log(fruit.replace('ban', '123'));
+console.log(fruit.toUpperCase());
+console.log(fruit.toLowerCase());
+console.log(Fruit.split('')); // (',')
