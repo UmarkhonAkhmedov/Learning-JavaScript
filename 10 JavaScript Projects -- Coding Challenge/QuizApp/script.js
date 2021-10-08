82eb8a1a38b86fdc8302c8cb5ae1b1464a24c745
UmarkhonAkhmedov <umarkhonakhmedov2000@gmail.com>
2021-10-08 06:55:31 +0500

    Adding html part of second project - Quiz App

---
+++ 10 JavaScript Projects -- Coding Challenge/QuizApp/script.js
@@ -0,0 +1,39 @@
+const quizData = [
+  {
+    questin: 'How old is Florin?',
+    a: '10',
+    b: '17',
+    c: '26',
+    d: '110',
+    coorect: 'c' 
+  }, {
+    question: 'What is the most used programming language in 2019?',
+    a: 'Java',
+    b: 'C',
+    c: 'Python',
+    d: 'JavaScript',
+    correct: 'd'
+  }, {
+    question: 'Who is he Presedent of US?',
+    a: 'Florin Pop',
+    b: 'Donald Trump',
+    c: 'Ivan Saldano',
+    d: 'Mihai Andrei',
+    coorect: 'b'
+  }, {
+    question: 'What does HTML stand for?',
+    a: 'Hypertext Markup Language',
+    b: 'Cascading Style Sheet',
+    c: 'Jason Object Notation',
+    d: 'Helicopters Terminals Motorboats Lamborigins',
+    correct: 'a'
+  },
+  {
+  question: 'What year was JavaScript launched?',
+  a: '1996',
+  b: '1995',
+  c: '1997',
+  d: 'none of the above',
+  correct: 'd'
+  }
+],
