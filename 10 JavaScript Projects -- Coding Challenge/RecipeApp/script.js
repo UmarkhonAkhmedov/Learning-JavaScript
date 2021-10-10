dff0a6e45c65f0c5fd614006feea023b393f384b
UmarkhonAkhmedov <umarkhonakhmedov2000@gmail.com>
2021-10-10 07:18:10 +0500

    Doing heart part of js with putting its info to local storage

--- 10 JavaScript Projects -- Coding Challenge/RecipeApp/script.js
+++ 10 JavaScript Projects -- Coding Challenge/RecipeApp/script.js
@@ -32,9 +32,39 @@ function addMeal(mealData, random = false){
     </div>
     <div class="meal-body">
         <h4>${mealData.strMeal}</h4>
-        <button class="fav-btn active"><i class="far fa-heart"></i></button>
+        <button class="fav-btn"><i class="fas fa-heart"></i></button>
     </div>
   `;
 
+  const btn = meal.querySelector('.meal-body .fav-btn');
+  btn.addEventListener("click", ()=>{
+    if(btn.classList.contains('active')){
+      removeMealLS(mealData.idMeal);
+      btn.classList.remove("active");
+    } else {
+      addMealLS(mealData.idMeal);
+      btn.classList.add("active");
+    }
+    
+  })
+
   meals.appendChild(meal);
+}
+
+function addMealLS(mealId){
+  const mealIds = getMealsLS();
+
+  localStorage.setItem('mealIds', JSON.stringify([...mealIds, mealId]));
+}
+
+function removeMealLS(mealId){
+  const mealIds = getMealsLS();
+
+  localStorage.setItem('mealIds', JSON.stringify(mealIds.filter((id) => id !== mealId)));
+}
+
+function getMealsLS(){
+  const mealIds = JSON.parse(localStorage.getItem('mealIds'));
+
+  return mealIds === null ? [] : mealIds;
 }
